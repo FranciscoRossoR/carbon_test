25582542a6d19a7f8908e3924d7d9d81bc9f01ff
FranciscoRossoR <franrosora@gmail.com>
2025-07-29 11:42:22 +0200

    feat: create base player panel

--- pages/store.ts
+++ pages/store.ts
@@ -1,6 +1,12 @@
 import GameState from "framework/entities/gameState";
+import Player from "framework/entities/player";
 import CarbonCityZeroState from "src/entities/carboncityzero/carbonCityZeroState";
 
-var gameState = new CarbonCityZeroState(1, 4, [], [], "open");
+const players = [new Player("player1"),
+    new Player("player2"),
+    new Player("player3")
+]
+
+var gameState = new CarbonCityZeroState(1, 4, players, [], "open");
 
 export default gameState;
--- src/components/layout/SummaryPanel.tsx
+++ src/components/layout/SummaryPanel.tsx
@@ -1,7 +1,9 @@
 import { Box, Flex, position, useDimensions } from "@chakra-ui/react";
+import Player from "framework/entities/player";
 import { observer } from "mobx-react";
 import gameState from "pages/store";
 import { useRef } from "react";
+import PlayerProfile from "../PlayerProfile";
 
 export interface IPanelProps {}
 
@@ -19,6 +21,14 @@ export default observer(function SummaryPanel(props: IPanelProps) {
     return (
         <Box ref={boxRef} __css={isMiniVersion ? sticky : {}} bgColor="brand.50">
             <Flex bgColor="brand.50" justifyContent="center">
+                {gameState.players.map((p: Player, index: number) => {
+                    const isCrurrent = true
+                    const info = new Map<string, string>()
+                    return (
+                        <PlayerProfile m="10px" key={"PlayerProfile" + p.name} name={p.name} color={p.color} info={info} active={isCrurrent}/>
+                    )
+                })
+                }
             </Flex>
         </Box>
     )
