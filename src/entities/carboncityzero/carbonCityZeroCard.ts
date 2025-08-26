1345782719afce6f1d1cfe536b22ac985af2059d
FranciscoRossoR <franrosora@gmail.com>
2025-08-26 18:56:51 +0200

    feat: add carbon attribute to card

--- src/entities/carboncityzero/carbonCityZeroCard.ts
+++ src/entities/carboncityzero/carbonCityZeroCard.ts
@@ -4,6 +4,7 @@ import { makeObservable, observable, action } from "mobx";
 export interface ICarbonCityZeroCard extends ICard {
     cost: number,
     income: number,
+    carbon: number
 }
 
 export class CarbonCityZeroCard extends Card {
@@ -12,13 +13,22 @@ export class CarbonCityZeroCard extends Card {
     cardAction?: () => void
     cost: number
     income: number
+    carbon: number
 
-    public constructor(name: string, hasAction: boolean = false, cardAction?: () => void, cost: number = 0, income: number = 0) {
+    public constructor(
+            name: string,
+            hasAction: boolean = false,
+            cardAction?: () => void,
+            cost: number = 0,
+            income: number = 0,
+            carbon: number = 0
+        ) {
         super(name)
         this.hasCardAction = hasAction
         if (this.hasCardAction) {this.cardAction = () => {alert("ACTION")}} // PLACEHOLDER
         this.cost = cost
         this.income = income
+        this.carbon = carbon
 
         makeObservable(this, {
             hasCardAction: observable,
