acaaafe73c610ac76121a9d814f782e5d94a4fa3
FranciscoRossoR <franrosora@gmail.com>
2025-08-28 12:58:30 +0200

    feat: add sector attribute to card

--- src/entities/carboncityzero/carbonCityZeroCard.ts
+++ src/entities/carboncityzero/carbonCityZeroCard.ts
@@ -1,6 +1,14 @@
 import Card, { ICard } from "framework/entities/card";
 import { makeObservable, observable, action } from "mobx";
 
+export enum Sector {
+    Starter = 0,
+    Industry,
+    Domestic,
+    Government,
+    Playtest
+}
+
 export interface ICarbonCityZeroCard extends ICard {
     cost: number,
     income: number,
@@ -9,26 +17,29 @@ export interface ICarbonCityZeroCard extends ICard {
 
 export class CarbonCityZeroCard extends Card {
 
-    hasCardAction: boolean
-    cardAction?: () => void
     cost: number
     income: number
     carbon: number
+    sector: Sector
+    hasCardAction: boolean
+    cardAction?: () => void
 
     public constructor(
             name: string,
-            hasAction: boolean = false,
-            cardAction?: () => void,
             cost: number = 0,
             income: number = 0,
-            carbon: number = 0
+            carbon: number = 0,
+            sector: Sector = Sector.Playtest,
+            hasAction: boolean = false,
+            cardAction?: () => void
         ) {
         super(name)
-        this.hasCardAction = hasAction
-        if (this.hasCardAction) {this.cardAction = () => {alert("ACTION")}} // PLACEHOLDER
         this.cost = cost
         this.income = income
         this.carbon = carbon
+        this.sector = sector
+        this.hasCardAction = hasAction
+        if (this.hasCardAction) {this.cardAction = () => {alert("ACTION")}} // PLACEHOLDER
 
         makeObservable(this, {
             hasCardAction: observable,
