823fe4d6e6ff73d04593869c79c4a60dc89e9c5f
FranciscoRossoR <franrosora@gmail.com>
2025-09-02 13:13:45 +0200

    feat: base add player function

--- pages/store.ts
+++ pages/store.ts
@@ -3,11 +3,6 @@ import Player from "framework/entities/player";
 import CarbonCityZeroPlayer from "src/entities/carboncityzero/carbonCityZeroPlayer";
 import CarbonCityZeroState from "src/entities/carboncityzero/carbonCityZeroState";
 
-const players = [new CarbonCityZeroPlayer("player1"),
-    new CarbonCityZeroPlayer("player2"),
-    new CarbonCityZeroPlayer("player3")
-]
-
-var gameState = new CarbonCityZeroState(players);
+var gameState = new CarbonCityZeroState();
 
 export default gameState;
