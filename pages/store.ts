4d7d7d6e64a25607d51527927bbf792a421e4676
FranciscoRossoR <franrosora@gmail.com>
2025-09-03 09:46:16 +0200

    feat: add basic connection

--- pages/store.ts
+++ pages/store.ts
@@ -1,8 +1,14 @@
+import { io } from "socket.io-client";
 import GameState from "framework/entities/gameState";
 import Player from "framework/entities/player";
 import CarbonCityZeroPlayer from "src/entities/carboncityzero/carbonCityZeroPlayer";
 import CarbonCityZeroState from "src/entities/carboncityzero/carbonCityZeroState";
 
-var gameState = new CarbonCityZeroState();
+const socket = io('http://localhost:8080')
+socket.on('connect', () => {
+    // Report connection
+    console.log(`You connected with ID: ${socket.id}`)
+})
 
+var gameState = new CarbonCityZeroState();
 export default gameState;
